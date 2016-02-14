2b32c3056af9d31da3666129abe1cf0572393323
bibi21000 <bibi21000@gmail.com>
2016-02-14 14:01:50 +0100

    Updates for raspberry

--- tests/test_server.py
+++ tests/test_server.py
@@ -52,8 +52,6 @@ COMMAND_DISCOVERY = 0x5000
 assert(COMMAND_DESC[COMMAND_DISCOVERY] == 'COMMAND_DISCOVERY')
 ##############################################################
 
-JNTTServer.skipRasperryTest()
-
 class TestPiSerser(JNTTServer, JNTTServerCommon):
     """Test the pi server
     """
